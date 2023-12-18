d975acc321f18934392fd04d177b13b35c401dc5
Rahomotul Islam (Shopnil) <shopnilkhan766@gmail.com>
2023-12-18 07:15:09 +0600

    Add files via upload

---
+++ Introductory Problems/Two Sets.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+#include<set>
+using namespace std;
+
+typedef long long ll;
+
+int main() {
+    ll n, sum = 0;
+    cin >> n;
+    ll x = n / 2, X = (n / 2) + 1;
+    ll h[x], H[X], result = (n * (n + 1)) / 2;
+    int a = 1, b = n;
+    set<int> printedNumbers;
+
+    if (result % 2 == 1) {
+        cout << "NO";
+    } else {
+        cout <<"YES\n"<< x << "\n";
+
+        for (int i = 1; i <= x; i++) {
+            cout << b << " ";
+            printedNumbers.insert(b);
+            sum += b;
+            b--;
+
+            if (sum == result / 2)
+                break;
+
+            cout << a << " ";
+            printedNumbers.insert(a);
+            sum += a;
+            a++;
+
+            if (sum == result / 2)
+                break;
+        }
+        if(n%2==1)
+        cout <<"\n"<< X << "\n";
+        else
+         cout <<"\n"<< X-1 << "\n";
+        for (int i = 1; i <= n; i++) {
+            if (printedNumbers.find(i) == printedNumbers.end()) {
+                cout << i << " ";            }
+        }
+    }
+
+    return 0;
+}
